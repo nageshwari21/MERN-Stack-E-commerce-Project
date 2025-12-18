d4d87c57b3cb641609e228dfa0b57c110fdc372b
NAGESHWARI <ghongadenisha@gmail.com>
2025-12-18 12:49:17 +0530

    Day 3: Implemented JWT authentication and middleware

--- config/db.js
+++ config/db.js
@@ -1,22 +1,13 @@
 import mongoose from "mongoose";
 import colors from "colors";
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(
-      `Connected To MongoDB Host: ${conn.connection.host}`.bgMagenta.white
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
-
-    // ✅ CORRECT place to log DB name
-    console.log(
-      `Connected Database Name: ${conn.connection.name}`.bgGreen.white
-    );
-
   } catch (error) {
-    console.log(`Error in MongoDB ${error}`.bgRed.white);
-    process.exit(1);
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
 };
 
