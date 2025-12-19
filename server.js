7d372b10319e1462b5d10bcbc6752c18f2b9e5b0
NAGESHWARI <ghongadenisha@gmail.com>
2025-12-19 15:00:54 +0530

    Day 4: Auth context, register, login UI and fixes

--- server.js
+++ server.js
@@ -2,37 +2,39 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 
-//configure env
+// config dotenv
 dotenv.config();
 
-//databse config
+// database config
 connectDB();
 
-//rest object
+// rest object
 const app = express();
 
-//middelwares
+// middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
+// routes
 app.use("/api/v1/auth", authRoutes);
 
-//rest api
+// test route
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+  res.status(200).send("<h1>Welcome to Ecommerce App</h1>");
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+    `Server Running in ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .white
   );
 });
